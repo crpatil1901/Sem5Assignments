675b0ad76147f381a0cab3543da59350ac10a3b5
Chinmay <crpatil1901@gmail.com>
2023-08-01 12:20:52 +0530

    OS Assignment1 Complete

--- OS/Assignment1/Main.java
+++ OS/Assignment1/Main.java
@@ -1,138 +1,99 @@
 package Assignment1;
 
-import java.util.*;;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        // Create a list of hardcoded jobs
-        List<Job> jobs = new ArrayList<>();
-        jobs.add(new Job("Job1", 0, 10, 3));
-        jobs.add(new Job("Job2", 1, 5, 2));
-        jobs.add(new Job("Job3", 2, 8, 1));
-        jobs.add(new Job("Job4", 3, 3, 4));
+class Job {
+    String name;
+    int arrivalTime;
+    int priority;
+    int busTime;
+    int remainingTime;
+    List<Integer> allocatedTimes;
+    int startTime;
+    int endTime;
+    int waitTime;
 
-        System.out.println("First-Come-First-Serve (FCFS):");
-        FirstComeFirstServe fcfsScheduler = new FirstComeFirstServe(new ArrayList<>(jobs));
-        fcfsScheduler.processJobs();
+    Job(String name, int arrivalTime, int priority, int busTime) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.priority = priority;
+        this.busTime = busTime;
+        this.remainingTime = busTime;
+        this.allocatedTimes = new ArrayList<>();
+        this.startTime = -1;
+        this.endTime = -1;
+        this.waitTime = 0;
+    }
 
-        System.out.println("\nShortest Job First (SJF):");
-        ShortestJobFirst sjfScheduler = new ShortestJobFirst(new ArrayList<>(jobs));
-        sjfScheduler.processJobs();
-
-        System.out.println("\nNon-preemptive Priority Scheduling:");
-        Priority priorityScheduler = new Priority(new ArrayList<>(jobs));
-        priorityScheduler.processJobs();
-
-        int timeQuantum = 3;
-        System.out.println("\nPreemptive Round-Robin Scheduling with time quantum " + timeQuantum + ":");
-        RoundRobin rrScheduler = new RoundRobin(new ArrayList<>(jobs), timeQuantum);
-        rrScheduler.processJobs();
+    void allocateTime(int time) {
+        allocatedTimes.add(time);
     }
 }
-
-
 
 class FirstComeFirstServe {
     List<Job> jobs;
 
     FirstComeFirstServe(List<Job> jobs) {
         this.jobs = jobs;
-        Collections.sort(jobs, new ArrivalTimeComparator());
+        Collections.sort(jobs, Comparator.comparingInt(job -> job.arrivalTime));
     }
 
     void processJobs() {
-        LinkedList<Job> queue = new LinkedList<Job>();
-        for (Job job : this.jobs) {
-            queue.add(job);
+        int currentTime = 0;
+        List<Job> completedJobs = new ArrayList<>();
+
+        for (Job job : jobs) {
+            currentTime = Math.max(currentTime, job.arrivalTime);
+            job.startTime = currentTime;
+            job.waitTime = currentTime - job.arrivalTime;
+
+            for (int t = 0; t < job.busTime; t++) {
+                job.allocatedTimes.add(currentTime + t);
+            }
+
+            currentTime += job.busTime;
+            job.endTime = currentTime;
+            completedJobs.add(job);
         }
-        int t = 0;
-        Job currentJob = null;
-        List<Job> completedJobs = new ArrayList<Job>();
-        while (!queue.isEmpty()) {
-            currentJob = queue.remove();
-            t = t > currentJob.arrivalTime ? t : currentJob.arrivalTime;
-            currentJob.startTime = t;
-            currentJob.responseTime = t;
-            currentJob.waitingTime = t - currentJob.arrivalTime;
-            t += currentJob.busTime;
-            currentJob.busLeft = 0;
-            currentJob.endTime = t;
-            completedJobs.add(currentJob);
-            currentJob = null;
-        }
-        Utilities.show(completedJobs);
+
+        Main.displayResults(completedJobs);
     }
 }
 
-class ShortestJobFirst {
+class NonPreemptivePriority {
     List<Job> jobs;
 
-    ShortestJobFirst(List<Job> jobs) {
+    NonPreemptivePriority(List<Job> jobs) {
         this.jobs = jobs;
-        Collections.sort(jobs, new ArrivalTimeComparator());
+        Collections.sort(jobs, (job1, job2) -> {
+            if (job1.arrivalTime == job2.arrivalTime) {
+                return job1.priority - job2.priority; // Sort by priority if arrival times are the same
+            }
+            return job1.arrivalTime - job2.arrivalTime; // Sort by arrival time
+        });
     }
 
     void processJobs() {
-        LinkedList<Job> queue = new LinkedList<>();
         int currentTime = 0;
-        Job currentJob = null;
         List<Job> completedJobs = new ArrayList<>();
+        PriorityQueue<Job> jobQueue = new PriorityQueue<>(Comparator.comparingInt(job -> job.priority));
 
-        while (!jobs.isEmpty() || !queue.isEmpty()) {
+        while (!jobs.isEmpty() || !jobQueue.isEmpty()) {
             while (!jobs.isEmpty() && jobs.get(0).arrivalTime <= currentTime) {
-                queue.add(jobs.remove(0));
+                jobQueue.add(jobs.remove(0));
             }
 
-            if (!queue.isEmpty()) {
-                Collections.sort(queue, new BurstTimeComparator());
+            if (!jobQueue.isEmpty()) {
+                Job currentJob = jobQueue.poll();
 
-                currentJob = queue.removeFirst();
-
-                if (currentJob.busTime > 0) {
-                    currentJob.busTime--;
-                    currentTime++;
-
-                    if (currentJob.busTime > 0) {
-                        queue.addFirst(currentJob);
-                    } else {
-                        currentJob.endTime = currentTime;
-                        completedJobs.add(currentJob);
-                    }
-                } else {
-                    currentJob.endTime = currentTime;
-                    completedJobs.add(currentJob);
+                if (currentJob.startTime == -1) {
+                    currentJob.startTime = currentTime;
                 }
-            } else {
-                currentTime++;
-            }
-        }
 
-        Utilities.show(completedJobs);
-    }
-}
+                for (int t = 0; t < currentJob.busTime; t++) {
+                    currentJob.allocateTime(currentTime + t); // Allocate each second the job runs
+                }
 
-class Priority {
-    List<Job> jobs;
-
-    Priority(List<Job> jobs) {
-        this.jobs = jobs;
-        Collections.sort(jobs, new PriorityComparator());
-    }
-
-    void processJobs() {
-        LinkedList<Job> queue = new LinkedList<>();
-        int currentTime = 0;
-        Job currentJob = null;
-        List<Job> completedJobs = new ArrayList<>();
-
-        while (!jobs.isEmpty() || !queue.isEmpty()) {
-            while (!jobs.isEmpty() && jobs.get(0).arrivalTime <= currentTime) {
-                queue.add(jobs.remove(0));
-            }
-
-            if (!queue.isEmpty()) {
-                currentJob = queue.removeFirst();
-                currentJob.startTime = currentTime;
                 currentTime += currentJob.busTime;
                 currentJob.endTime = currentTime;
                 completedJobs.add(currentJob);
@@ -141,126 +102,153 @@ class Priority {
             }
         }
 
-        Utilities.show(completedJobs);
+        Main.displayResults(completedJobs);
     }
 }
 
-class RoundRobin {
-    List<Job> jobs;
-    int timeQuantum;
 
-    RoundRobin(List<Job> jobs, int timeQuantum) {
+class PreemptiveShortestJobFirst {
+    List<Job> jobs;
+
+    PreemptiveShortestJobFirst(List<Job> jobs) {
         this.jobs = jobs;
-        this.timeQuantum = timeQuantum;
-        Collections.sort(jobs, new ArrivalTimeComparator());
+        Collections.sort(jobs, Comparator.comparingInt(job -> job.arrivalTime));
     }
 
     void processJobs() {
-        LinkedList<Job> queue = new LinkedList<>();
         int currentTime = 0;
-        Job currentJob = null;
         List<Job> completedJobs = new ArrayList<>();
+        PriorityQueue<Job> jobQueue = new PriorityQueue<>(Comparator.comparingInt(job -> job.remainingTime));
 
-        while (!jobs.isEmpty() || !queue.isEmpty()) {
+        while (!jobs.isEmpty() || !jobQueue.isEmpty()) {
             while (!jobs.isEmpty() && jobs.get(0).arrivalTime <= currentTime) {
-                queue.add(jobs.remove(0));
+                jobQueue.add(jobs.remove(0));
             }
 
-            if (!queue.isEmpty()) {
-                currentJob = queue.removeFirst();
+            if (!jobQueue.isEmpty()) {
+                Job currentJob = jobQueue.poll();
+                int burstTime = Math.min(currentJob.remainingTime, 1);
 
-                int remainingTime = currentJob.busTime;
-                if (remainingTime > timeQuantum) {
-                    currentJob.busTime -= timeQuantum;
-                    currentTime += timeQuantum;
-                    queue.addFirst(currentJob);
-                } else {
-                    currentTime += remainingTime;
+                if (currentJob.startTime == -1) {
+                    currentJob.startTime = currentTime;
+                }
+
+                for (int t = 0; t < burstTime; t++) {
+                    currentJob.allocateTime(currentTime + t); // Allocate each second the job runs
+                }
+
+                currentJob.remainingTime -= burstTime;
+                currentTime += burstTime;
+
+                if (currentJob.remainingTime == 0) {
                     currentJob.endTime = currentTime;
                     completedJobs.add(currentJob);
+                } else {
+                    jobQueue.add(currentJob);
                 }
             } else {
                 currentTime++;
             }
         }
 
-        Utilities.show(completedJobs);
+        Main.displayResults(completedJobs);
     }
 }
 
+class PreemptiveRoundRobin {
+    List<Job> jobs;
+    int timeQuantum;
 
-class Utilities {
-    static Scanner sc = new Scanner(System.in);
-    static Job userInput() {
-        String name = sc.next();
-        int arrivalTime = sc.nextInt();
-        int busTime = sc.nextInt();
-        int priority = sc.nextInt();
-        sc.reset();
-        Job newJob = new Job(name, arrivalTime, busTime, priority);
-        return newJob;
+    PreemptiveRoundRobin(List<Job> jobs, int timeQuantum) {
+        this.jobs = jobs;
+        this.timeQuantum = timeQuantum;
+        Collections.sort(jobs, Comparator.comparingInt(job -> job.arrivalTime));
     }
 
-    static void show(List<Job> jobs) {
-        for (Job job : jobs) {
-            System.out.println(job);
+    void processJobs() {
+        int currentTime = 0;
+        List<Job> completedJobs = new ArrayList<>();
+        LinkedList<Job> jobQueue = new LinkedList<>();
+
+        while (!jobs.isEmpty() || !jobQueue.isEmpty()) {
+            while (!jobs.isEmpty() && jobs.get(0).arrivalTime <= currentTime) {
+                jobQueue.add(jobs.remove(0));
+            }
+
+            if (!jobQueue.isEmpty()) {
+                Job currentJob = jobQueue.poll();
+                int burstTime = Math.min(currentJob.remainingTime, timeQuantum);
+
+                if (currentJob.startTime == -1) {
+                    currentJob.startTime = currentTime;
+                }
+
+                for (int t = 0; t < burstTime; t++) {
+                    currentJob.allocateTime(currentTime + t); // Allocate each second the job runs
+                }
+
+                currentJob.remainingTime -= burstTime;
+                currentTime += burstTime;
+
+                if (currentJob.remainingTime <= 0) {
+                    currentJob.endTime = currentTime;
+                    completedJobs.add(currentJob);
+                } else {
+                    jobQueue.add(currentJob);
+                }
+            } else {
+                currentTime++;
+            }
         }
+
+        Main.displayResults(completedJobs);
     }
 }
 
-class Job implements Comparable<Job> {
-    String name;
-    int arrivalTime;
-    int busTime;
-    int priority;
-    int startTime;
-    int endTime;
-    int responseTime;
-    int waitingTime;
-    int busLeft;
+public class Main {
 
-    Job(String name, int arrivalTime, int busTime, int priority) {
-        this.name = name;
-        this.arrivalTime = arrivalTime;
-        this.busTime = busTime;
-        this.priority = priority;
-        this.startTime = -1;
-        this.endTime = -1;
-        this.responseTime = -1;
-        this.waitingTime = 0;
-        this.busLeft = busTime;
+    static List<Job> getJobs() {
+        List<Job> jobs = new ArrayList<>();
+        jobs.add(new Job("Job1", 0, 3, 10));
+        jobs.add(new Job("Job2", 1, 2, 5));
+        jobs.add(new Job("Job3", 2, 1, 8));
+        jobs.add(new Job("Job4", 3, 4, 3));
+        return jobs;
+    }
+    public static void main(String[] args) {
+        
+        List<Job> jobs = getJobs();
+
+        System.out.println("First-Come-First-Serve (FCFS):");
+        FirstComeFirstServe fcfsScheduler = new FirstComeFirstServe(new ArrayList<>(jobs));
+        fcfsScheduler.processJobs();
+
+        jobs = getJobs();
+
+        System.out.println("\nNon-preemptive Priority Scheduling:");
+        NonPreemptivePriority npPriorityScheduler = new NonPreemptivePriority(new ArrayList<>(jobs));
+        npPriorityScheduler.processJobs();
+
+        jobs = getJobs();
+
+        System.out.println("\nPreemptive Shortest Job First (SJF):");
+        PreemptiveShortestJobFirst sjfScheduler = new PreemptiveShortestJobFirst(new ArrayList<>(jobs));
+        sjfScheduler.processJobs();
+
+        jobs = getJobs();
+
+        int timeQuantum = 3;
+        System.out.println("\nPreemptive Round-Robin Scheduling with time quantum " + timeQuantum + ":");
+        PreemptiveRoundRobin rrScheduler = new PreemptiveRoundRobin(new ArrayList<>(jobs), timeQuantum);
+        rrScheduler.processJobs();
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s:\t%d\t%d\t%d\t%d\t%d\t%d\t%d", name, arrivalTime, busTime, priority, startTime, endTime, responseTime, waitingTime);
-    }
-
-    @Override
-    public int compareTo(Job o) {
-        return o.arrivalTime;
-    }
-}
-
-class ArrivalTimeComparator implements Comparator<Job> {
-
-    @Override
-    public int compare(Job a, Job b) {
-        return a.arrivalTime - b.arrivalTime;
-    }
-    
-}
-
-class BurstTimeComparator implements Comparator<Job> {
-    @Override
-    public int compare(Job a, Job b) {
-        return a.busTime - b.busTime;
-    }
-}
-
-class PriorityComparator implements Comparator<Job> {
-    @Override
-    public int compare(Job a, Job b) {
-        return a.priority - b.priority;
+    static void displayResults(List<Job> jobs) {
+        System.out.println("Name\tAT\tPriority\tBT\tStart\tEnd\tWait\tAllocated Times");
+        for (Job job : jobs) {
+            int totalWait = job.endTime - job.arrivalTime - job.busTime;
+            System.out.print(job.name + "\t" + job.arrivalTime + "\t" + job.priority + "\t\t" + job.busTime + "\t" + job.startTime + "\t" + job.endTime + "\t" + totalWait + "\t");
+            System.out.println(job.allocatedTimes);
+        }
     }
 }
