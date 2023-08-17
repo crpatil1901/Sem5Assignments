b73e934966b46a89c87038b856bee7930c1b2a3c
crpatil1901 <crpatil1901@gmail.com>
2023-08-17 11:37:39 +0530

    CNHamming

--- CN/assignment3.py
+++ CN/assignment3.py
@@ -1,10 +1,13 @@
+from random import randint
+
 EVEN, ODD = True, False
-MODE = EVEN
+MODE = ODD
 
 def encodeHamming(data, mode = MODE):
     data = data[::-1]
     m, r = len(data), 0
     while 2**r < m + r + 1 : r += 1
+    print("No. of redundant bits: ", r)
     bits = [0 for _ in range(m+r)]
     i = 0
     j = 0
@@ -14,7 +17,7 @@ def encodeHamming(data, mode = MODE):
         bits[j] = data[i]; i += 1; j += 1
     # print(bits[::-1])
     for i in range(r):
-        j, flag, cntr, parity = 2**i - 1, mode, 2**i, mode
+        j, flag, cntr, parity = 2**i - 1, True, 2**i, mode
         while j < m+r:
             if flag: parity = parity != bits[j]; # print(j+1, end=" ")
             cntr -= 1
@@ -31,13 +34,11 @@ def decodeHamming(data, mode = MODE):
     while 2**r < n: r += 1
     for i in range(r):
         parity = mode
-        j, cntr, flag = 2**i - 1, 2**i, mode
+        j, cntr, flag = 2**i - 1, 2**i, True
         while j < n:
-            if flag: parity = parity != data[j]; # print(j+1, end=" ")
-            cntr -= 1
+            if flag: parity = parity != data[j]
             if cntr == 0: flag = not flag; cntr = 2**i
             j += 1
-        # print()
         errorPosition.append(parity)
     if any(errorPosition):
         pos = 0
@@ -45,11 +46,10 @@ def decodeHamming(data, mode = MODE):
             pos *= 2
             pos += 1 if position else 0
         pos -= 1
-        if pos < n: data[pos] = data[pos] != data[pos]
+        if pos < n: data[n - 1 -pos] = data[n - 1 - pos] != data[n - 1 - pos]
     for i in range(r-1, -1, -1):
         data.pop(2**i - 1)
     return data[::-1]
-
 
 def convertToBits(char):
     ascii = ord(char)
@@ -71,14 +71,39 @@ def convertFromBits(data):
 # decoded = decodeHamming(encoded)
 # print(decoded)
 
-message = input("Enter Message: ")
-print("\nString: ")
-charAsBits = list(map(lambda x: convertToBits(x), message))
-print("\nEncoded Hamming: ")
-encodedString = list(map(lambda x: encodeHamming(x), charAsBits))
-print(*encodedString, sep="\n")
-recieved = list(map(lambda x: decodeHamming(x), encodedString))
-print("\nRecieved Hamming: ")
-print(*recieved, sep="\n")
-decodedMessage = "".join(list(map(lambda x: convertFromBits(x), recieved)))
-print("\nDecoded Message:", decodedMessage)
+while True:
+    i = input("Enter 1 for EVEN parity, 0 for ODD parity: ")
+    if i == '1':
+        MODE = EVEN; break
+    elif i == '0':
+        MODE = ODD; break
+    else:
+        print("Invalid Input.")
+
+if __name__ == "__main__":
+    message = input("Enter Message: ")
+    print("\nString: ")
+
+    charAsBits = list(map(lambda x: convertToBits(x), message))
+    print("\nEncoded Hamming: ")
+
+    encodedString = list(map(lambda x: encodeHamming(x), charAsBits))
+    print(*encodedString, sep="\n")
+
+    e = input("Do you want to induce error? (Y/any)")
+
+    if e == 'y' or e == 'Y':
+        char = 2 # randint(0, len(message) - 1)
+        flippedBit = 3 # randint(0, len(encodedString) - 1)
+        print("Flipping bit", flippedBit, "of character", message[char])
+        encodedString[char][flippedBit] = not encodedString[char][flippedBit]
+        print(*encodedString, sep="\n")
+        # encodedString[char][flippedBit] = not encodedString[char][flippedBit]
+
+    recieved = list(map(lambda x: decodeHamming(x), encodedString))
+    print("\nRecieved Hamming: ")
+
+    print(*recieved, sep="\n")
+    decodedMessage = "".join(list(map(lambda x: convertFromBits(x), recieved)))
+    
+    print("\nDecoded Message:", decodedMessage)
